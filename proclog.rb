db6ea8a53de44cebfa06b3518f024f3c5987ca77
Maxime Ransan <maxime.ransan@gmail.com>
2010-09-26 20:54:28 +0100

    remove non genric code.

--- proclog.rb
+++ proclog.rb
@@ -3,6 +3,7 @@
 require 'yaml'
 
 class ProcLog
+    
     PIPE_DIR  = "/tmp/" 
     PIPE_EXT  = ".fifo"
     
@@ -97,102 +98,16 @@ class ProcLog
 
 end 
 
-#def run_as_test_child child_name
-#    pipe_io = File::open ProcLog::pipe_name_for(child_name), "w"
-#    while true
-#        puts "I am process #{Process.pid}"
-#        pipe_io.puts "To pipe from process #{Process.pid}"
-#        pipe_io.flush
-#        Kernel.sleep(1)
-#    end 
-#end
-
-class MbigCfg
-    MBIG_TSK = "mbig.tsk"
-
-    attr_reader :name
-    attr_reader :cmd
-    attr_reader :working_dir
-    
-    def initialize yaml
-        @name = "mbig"
-        @mbig_number = yaml['mbig_number']
-        throw "Invalid MBIG" if @mbig_number.nil?
-
-        @working_dir = IO.popen("pwd").gets.chomp!
-        @cmd = [MBIG_TSK,  @mbig_number]
-    end
+def run_as_test_child child_name
+    pipe_io = File::open ProcLog::pipe_name_for(child_name), "w"
+    while true
+        puts "I am process #{Process.pid}"
+        pipe_io.puts "To pipe from process #{Process.pid}"
+        pipe_io.flush
+        Kernel.sleep(1)
+    end 
 end
 
-class ServiceCfg
-    TASK_EXT = 'tsk'
-    CFG_EXT  = 'cfg'
-    BB_DIR   = '/bb/bin/'        
-    
-    attr_reader :name
-    attr_reader :task
-    attr_reader :cfg
-    attr_reader :working_dir
-
-    def initialize name, yaml_obj
-        @name     = name
-        cfg_dir   = yaml_obj['cfg_dir']
-        build_dir = yaml_obj['build_dir']  
- 
-        @local = !yaml_obj['run_on']['local'].nil? and 
-                  yaml_obj['run_on']['local']
-        if @local
-            @working_dir = build_dir
-            
-            @task = "./#{@name}.sundev1.#{TASK_EXT}" 
-            @cfg  = "./#{@name}.#{CFG_EXT}"
-        else
-            version = yaml_obj['run_on']['version']
-            version = "" if version == 1
-            @workin_dir = IO.popen("pwd").gets.chomp! 
-            @task   = "#{BB_DIR}/#{@name}.#{TASK_EXT}"
-            @cfg    = "#{BB_DIR}/#{@name}.#{CFG_EXT}"
-        end
-    end
-
-    def cmd
-        [task, cfg, "-l", ProcLog::pipe_name_for(name)]
-    end
-end
-
-class Config
-    CFG_FILE = "proclog.cfg"
-    
-    attr_reader :processes 
-    def initialize
-        @processes =  []
-        File::open(CFG_FILE) do |cfg_file|
-            yaml = YAML::load(cfg_file)
-            
-            if not yaml['services'].nil?
-                yaml['services'].each_pair do |service_name, service_cfg|
-                    @processes.push ServiceCfg.new(service_name, service_cfg)
-                end 
-            end 
-            
-            if not yaml['mbig'].nil?
-                @processes.push  MbigCfg.new(yaml['mbig'])
-            end
-        end 
-    end
-end
-        
-config  = Config.new
-proclog = ProcLog.new
-
-config.processes.each do |proc| 
-    
-    proclog.add_child proc.name do |child_name|
-        puts "Changing to dir:#{proc.working_dir}"
-        Dir.chdir proc.working_dir
-        Kernel.exec(*proc.cmd)
-    end
-end
 
 proclog.run
 
